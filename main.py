901118074ca0a1dde422cf0e4793dbbc86a7d2cc
mynameisanton <252355343+vasya666-tech@users.noreply.github.com>
2026-07-24 12:21:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, 
 from os import getenv
 from dotenv import load_dotenv
 import psycopg2
+from requests import get
 
 load_dotenv()
 DB = getenv('DB_URL')
@@ -37,9 +38,37 @@ def trade():
     city_name = data.get("message")
     if city_name:
         city_name = city_name.title()
+    geo = get(
+        "https://geocoding-api.open-meteo.com/v1/search",
+        params={
+            "name": city_name,
+            "count": 1,
+            "language": "en"
+            }
+        ).json()
+    latitude = geo['results'][0]['latitude']
+    longitude = geo['results'][0]['longitude']
+    
+    weather = get(
+        "https://api.open-meteo.com/v1/forecast",
+        params={
+            "latitude": latitude,
+            "longitude": longitude,
+            "current": "temperature_2m"
+            }
+        ).json()
+
+    temperature = f"{weather['current']['temperature_2m']}°C"
     population = connection.execute(select(city.c.population).where(city.c.name == city_name)).scalar()
     description = connection.execute(select(city.c.description).where(city.c.name == city_name)).scalar()
     image = connection.execute(select(city.c.image).where(city.c.name == city_name)).scalar()
     if population is not None and description is not None:
-        return jsonify({"name": city_name, "population": population, "description": description, "image": image})
-    return jsonify({"error": "Try another city, please."}), 404
+        return jsonify({
+            "name": city_name,
+            "population": population,
+            "description": description,
+            "image": image,
+            "temperature": temperature
+            }
+                      )
+        return jsonify({"error": "Try another city, please."}), 404
