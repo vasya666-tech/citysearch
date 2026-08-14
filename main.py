fa70308bdf1b145a394d49400101302f6bd23de9
mynameisanton <252355343+vasya666-tech@users.noreply.github.com>
2026-08-15 00:37:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify, make_response, redirect, url_for
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, select
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, select, update
 from os import getenv
 from dotenv import load_dotenv
 import psycopg2
@@ -31,7 +31,8 @@ users = Table(
     metadata,
     Column("id", Integer, primary_key=True),
     Column("username", String),
-    Column("password_hash", String)
+    Column("password_hash", String),
+    Column("flights", String)
     )
  
 app = Flask(__name__)
@@ -50,9 +51,8 @@ def main():
 @app.get('/login')
 def login_page():
     token = request.cookies.get("token")
-    if token:
-        return redirect(url_for('profile_page'))
-    return render_template('login.html')
+    if not token:
+        return render_template('login.html')
 
 @app.post('/reg')
 def reg():
@@ -105,17 +105,25 @@ def login():
 def profile_page():
     token = request.cookies.get("token")
     if not token:
-        return redirect(url_for('main')) 
+        return redirect(url_for('main'))
     try:
         data = jwt.decode(token, KEY, algorithms="HS256")
         user_id = data["user_id"]
-        username = connection.execute(select(users.c.username).where(users.c.id == user_id)).first()
-        return render_template("index.html", username=username[0], logout=True)
-
+        user_data = connection.execute(select(
+            users.c.username,
+            users.c.flights).where(users.c.id == user_id)).mappings().first()
+        return render_template("index.html",
+                               username=user_data["username"],
+                               flights=user_data["flights"].strip(','),
+                               logout=True, button=True)
     except jwt.ExpiredSignatureError:
-        return jsonify({"error": "Срок действия токена истек"}), 401
+        response = make_response(redirect(url_for("login")))
+        response.delete_cookie("token")
+        return response
     except jwt.InvalidTokenError:
-        return jsonify({"error": "Недействительный токен"}), 401
+        response = make_response(redirect(url_for("main")))
+        response.delete_cookie("token")
+        return response
 
 @app.get("/logout")
 def logout():
@@ -123,12 +131,50 @@ def logout():
     response.set_cookie("token", " ", max_age=0, expires=0, path="/")
     return response
 
+@app.post('/save')
+def save():
+    token = request.cookies.get("token")
+    try:
+        data = jwt.decode(token, KEY, algorithms="HS256")
+        user_id = data["user_id"]
+        flights_db = connection.execute(select(users.c.flights).where(users.c.id==user_id)).scalar()
+    except jwt.ExpiredSignatureError:
+        response = make_response(redirect(url_for("login")))
+        response.delete_cookie("token")
+        return response
+    except jwt.InvalidTokenError:
+        response = make_response(redirect(url_for("main")))
+        response.delete_cookie("token")
+        return response
+    
+        
+    data = request.get_json()
+    city_name = data.get("message").strip().title()
+    city_name_db = connection.execute(select(city.c.name).where(city.c.name==city_name)).scalar()
+    if city_name_db == city_name:
+        print(flights_db.split(','))
+        if city_name_db not in flights_db.split(','):
+            travel_list = f"{flights_db}{city_name_db},"
+            set_flights = connection.execute(update(users).where(users.c.id==user_id).values(flights=travel_list))
+            connection.commit()
+            return jsonify({
+                "message": f"You just added {city_name_db} to your travel list.",
+                "travel_list": travel_list.strip(',')})
+        return jsonify({"message": f"{city_name_db} is already in your travel list."})
+    return jsonify({"message": f"City not found."})
+
 @app.post('/trade')
 def trade():
     data = request.get_json()
-    city_name = data.get("message")
-    if city_name:
-        city_name = city_name.title()
+    city_name = data.get("message").strip().title()
+    
+    city_data = connection.execute(select(
+        city.c.population,
+        city.c.description,
+        city.c.image).where(city.c.name == city_name)).mappings().first()
+    if city_data is None:
+        return jsonify({"error": "Try another city, please."})
+    
     geo = get(
         "https://geocoding-api.open-meteo.com/v1/search",
         params={
@@ -157,17 +203,13 @@ def trade():
         weather_response.raise_for_status()
         weather = weather_response.json()
         temperature = f"{weather['current']['temperature_2m']}°C"
-    population = connection.execute(select(city.c.population).where(city.c.name == city_name)).scalar()
-    description = connection.execute(select(city.c.description).where(city.c.name == city_name)).scalar()
-    image = connection.execute(select(city.c.image).where(city.c.name == city_name)).scalar()
     
-    if population is not None and description is not None:
-        return jsonify({
-            "name": city_name,
-            "population": population,
-            "description": description,
-            "image": image,
-            "temperature": temperature
-            }
-                       )
-    return jsonify({"error": "Try another city, please."}), 404
+    return jsonify({
+        "name": city_name,
+        "population": city_data["population"],
+        "description": city_data["description"],
+        "image": city_data["image"],
+        "temperature": temperature,
+        "visible": True
+        }
+                   )
